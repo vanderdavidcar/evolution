9151636b13da3b71f592cde4e5b7c93e6cd21e76
root <vanderdavidcar>
2022-03-17 23:17:57 -0300

    Update IOS-XR

--- test_nxos_version.py
+++ test_nxos_version.py
@@ -1,11 +1,7 @@
 import pytest
 
 nxos_output = """
-    "vendor": "Cisco",
-    "serial_number": "FDO23350U1E",
-    "model": "Nexus9000 93180YC-EX chassis",
-    "hostname": "brbsa-bt02-repl2",
-    "os_version": "9.2(3)",
+"os_version": "9.2(3)",
 """
 print(nxos_output)
 
@@ -15,10 +11,7 @@ assert nxos_data == "9.2(3)"
 
 
 nxos_output = """
-    "vendor": "Cisco",
-    "serial_number": "FDO23350U1E",
-    "model": "Nexus9000 93180YC-EX chassis",
-    "hostname": "brbsa-bt02-repl2",
+"os_version": "10",
 """
 print(nxos_output)
 nxos_data = "9.2(3)"
