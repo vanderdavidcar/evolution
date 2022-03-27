4f3e3d2e2f07249b146efe6c9b64eb2d31963d0e
root <vanderdavidcar>
2022-03-27 19:58:35 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 # .gitignore should include reference to config.py
-api_key = ""
-nb_username = ""
-nb_password = ""
+api_key = "27e9dba2aed27f0f38f6a7f1a29b4f7750223a41"
+nb_username = "admin"
+nb_password = "admin"
+nb_url = "https://demo.netbox.dev/"
