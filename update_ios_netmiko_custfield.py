8cf6d8530c859b1ff97c47f486a01a4b175dfc67
root <vanderdavidcar>
2022-04-13 10:50:15 -0300

    upload netmiko update sw_version

--- update_ios_netmiko_custfield.py
+++ update_ios_netmiko_custfield.py
@@ -30,21 +30,21 @@ Custom field to update: "sw_version"
 Assume network device list would contain Cisco Catalyst IOS
 """
 # List of devices vendors "NXOS"
-ios_model = list(nb.dcim.devices.filter(model="iol"))
+ios_model = list(nb.dcim.devices.filter(model="ios"))
 print(ios_model)
 
 for devices in ios_model:
     print('Connecting to device" ' + str(devices))
     ios_device = {
-        'device_type': 'cisco_ios',
-        'ip': str(devices),
-        'username': config.nb_username,
-        'password': config.nb_password
+        "device_type": "cisco_ios",
+        "ip": str(devices),
+        "username": config.nb_username,
+        "password": config.nb_password,
     }
     net_connect = ConnectHandler(**ios_device)
-    sh_ver = net_connect.send_command('show version')
+    sh_ver = net_connect.send_command("show version")
 
-    version_pattern = re.compile(r'Version (?P<version>\S........)')
+    version_pattern = re.compile(r"Version (?P<version>\S........)")
     ios_version = version_pattern.search(sh_ver)
 
     # Print regex information collect ina file ios_version.txt
@@ -55,7 +55,7 @@ for devices in ios_model:
     # Retrieve router object for update with dictionary
     for devices in ios_model:
         # Update custom fields "sw_version" using regex information collected
-        dict(devices)['custom_fields']["sw_version"] = version
+        dict(devices)["custom_fields"]["sw_version"] = version
         devices.save()
         break
 
@@ -63,4 +63,3 @@ for devices in ios_model:
     print("Device Type: ", devices.device_type)
     print("sw_version: ", devices.custom_fields["sw_version"])
     print("Current tenant: ", devices.tenant)
-    
